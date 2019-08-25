919d6dfbf4f419c445da786f2a7ced8715a05c7c
pyw0564 <pyw0564@gmail.com>
2019-08-25 15:59:43 +0900

    create notify-inquire and update directory-root

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const sscanf = require('scanf').sscanf;
 
 var app = express();
 // 인증 토큰 발급 받기
-app.use('/inspect', express.static('src/inspect'));
-app.use('/unpaid', express.static('src/unpaid'));
+app.use('/inspect', express.static('src/joynoly/inspect'));
+app.use('/unpaid', express.static('src/joynoly/unpaid'));
 app.use('/', express.static(__dirname));
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -17,7 +17,7 @@ app.locals.pretty = true;
 app.get('/', function(req, res) {
   let data = {};
   // 검침
-  let s = fs.readFileSync('inspect/inspect_list.txt', {
+  let s = fs.readFileSync('joynoly/inspect/inspect_list.txt', {
     encoding: 'utf8'
   }, function(err, s) {});
 
@@ -42,7 +42,7 @@ app.get('/', function(req, res) {
   }
 
   // 미납
-  s = fs.readFileSync('unpaid/unpaid_list.txt', {
+  s = fs.readFileSync('joynoly/unpaid/unpaid_list.txt', {
     encoding: 'utf8'
   }, function(err, s) {});
 
@@ -66,7 +66,34 @@ app.get('/', function(req, res) {
     });
   }
 
-  s = fs.readFileSync('clear_list.txt', {
+  // 고지
+  s = fs.readFileSync('joynoly/notify/notify_list.txt', {
+    encoding: 'utf8'
+  }, function(err, s) {});
+
+  data.list_notify = [];
+  while (s) {
+    let ret = getline(s);
+    // 주석처리
+    if (ret[0] == '#') {
+      s = sscanf_nomalize(s, ret);
+      continue;
+    }
+    ret = sscanf(s, '%s %s %s', 'item', 'name', 'url');
+    for (let i in ret) {
+      ret[i] = nomalize_back(ret[i]);
+      s = sscanf_nomalize(s, ret[i]);
+    }
+    data.list_notify.push({
+      item: ret.item,
+      name: ret.name,
+      url: ret.url
+    });
+  }
+
+
+  // 취소
+  s = fs.readFileSync('joynoly/clear_list.txt', {
     encoding: 'utf8'
   }, function(err, s) {});
   data.list_clear = [];
